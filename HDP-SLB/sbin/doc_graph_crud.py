64814af8e1af3833715ba870b74522d143187349
praveensmriti <praveensmriti@gmail.com>
2015-03-14 15:27:24 -0700

    updated

--- HDP-SLB/sbin/doc_graph_crud.py
+++ HDP-SLB/sbin/doc_graph_crud.py
@@ -59,14 +59,32 @@ def _get_client_connection(conf_dict):
     return _client_handle
 
 
-def _checkin_json_doc(client_handle, json_string):
-    _create_db_objects   
- 
+def _create_db_graph_objects(c_handle):
+    try:
+        # Create SLB graph database
+        c_handle.db_create("WellSurveyGraph", pyorient.DB_TYPE_GRAPH, pyorient.STORAGE_TYPE_PLOCAL)
+        c_handle.db_open("WellSurveyGraph", "admin", "admin")
 
-def _do_action(checkin_json,artifact_name):
+        # Create SLB graph vertexs
+        c_handle.command('create class Wells extends V')
+        c_handle.command('create class Survey extends V')
+        c_handle.command('create class survey_belongs_to extends E')
+    except Exception as e:
+        print color("Error creating graph database objects : " + e.message, 'red')
+        sys.exit(1)
+    return 'Success'
+
+
+def _checkin_json_doc(client_handle, json_string):
+    _return_flag = _create_db_graph_objects(client_handle)
+
+    
+
+def _do_action(arguments,checkin_json,artifact_name):
     _config_dict = _initialize_credentials()
     _client_handle = _get_client_connection(_config_dict)
-    _record_id = _checkin_json_doc(_client_handle, checkin_json)
+    _record_id = _checkin_json_doc(_client_handle, arguments.checkin_doc, arguments.checkin_type)
+    _record_id = _checkout_json_doc(_client_handle, arguments.artifact_name, arguments.checkout_type)
 
     print 'Processing checkin and checkout of artifacts'
 
@@ -98,6 +116,11 @@ def parse_cl_options():
         dest="checkout_type",
         required=False,
         help="Specify the artifact checkout artifact type e.g wells or survey")
+    parser.add_argument(
+        "--parent-name",
+        dest="parent_name",
+        required=False,
+        help="Specify the parent name for the artifact")
 
     return parser
 
@@ -106,7 +129,7 @@ def main():
     parser = parse_cl_options()
     args = parser.parse_args()
     if args.checkin_doc or args.artifact_name:
-        success_flag = _do_action(args.checkin_doc,args.artifact_name)
+        success_flag = _do_action(args)
     else:
         parser.print_help()
 
