399c8a9c157980a0af7162127947c057297d41a0
praveensmriti <praveensmriti@gmail.com>
2015-03-30 14:05:56 -0700

    updated

--- HDP-SLB/sbin/slb_rest_server.py
+++ HDP-SLB/sbin/slb_rest_server.py
@@ -19,7 +19,11 @@ def abort_if_artifact_doesnt_exist(artifact_id):
 
 @app.route('/')
 def homePage():
-    return render_template("index1.html")
+    return render_template("index.html")
+
+@app.route('/graph')
+def graphPage():
+    return render_template("graph.html")
 
 class Artifact(Resource):
     def get(self, artifact_id):
